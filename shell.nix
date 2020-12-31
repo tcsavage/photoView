2a315682b095d2bb27ba2d7d4ed3e1bafe8738ce
Tom Savage <tcsavage@gmail.com>
2020-12-31 12:35:04 +0000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+ openimageio2 = with pkgs ; import ./nix/openimageio2.nix { inherit pkgs; };
+in pkgs.stdenvNoCC.mkDerivation {
+  name = "photo-view-gcc-environment";
+  buildInputs = [
+      pkgs.gcc10
+      pkgs.gdb
+      pkgs.pkg-config
+      pkgs.cmake
+      openimageio2
+      pkgs.libraw
+      pkgs.openexr
+      pkgs.qt5.qtbase
+      pkgs.flatpak-builder
+  ];
+}
