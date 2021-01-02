36bbc54640a6029d20828998aa0e051bbde3b184
Tom Savage <tcsavage@gmail.com>
2021-01-02 16:55:18 +0000

    Mark all color functions as constexpr

--- libs/image/libimage/include/image/Color.hpp
+++ libs/image/libimage/include/image/Color.hpp
@@ -22,37 +22,37 @@ namespace image {
 
     template <class In, class Out>
     requires (std::unsigned_integral<In> && std::unsigned_integral<Out>) || (std::floating_point<In> && std::floating_point<Out>)
-    Out conv(const In &in) {
+    constexpr Out conv(const In &in) {
         return in;
     }
 
     template <std::unsigned_integral In, std::floating_point Out>
-    Out conv(const In &in) {
+    constexpr Out conv(const In &in) {
         return static_cast<Out>(in) / static_cast<Out>(std::numeric_limits<In>::max());
     }
 
     template <std::floating_point In, std::unsigned_integral Out>
-    Out conv(const In &in) {
+    constexpr Out conv(const In &in) {
         return static_cast<Out>(in * static_cast<In>(std::numeric_limits<Out>::max()));
     }
 
     template <class In, class Out>
-    ColorRGB<Out> conv(const ColorRGB<In> &in) {
+    constexpr ColorRGB<Out> conv(const ColorRGB<In> &in) {
         return ColorRGB<Out> { conv<In, Out>(in.r), conv<In, Out>(in.g), conv<In, Out>(in.b) };
     }
 
     template <std::floating_point T>
-    T mix(T factor, T a, T b) {
+    constexpr T mix(T factor, T a, T b) {
         return ((1 - factor) * a) + (factor * b);
     }
 
     template <std::unsigned_integral T, std::floating_point F = F32>
-    T mix(F factor, T a, T b) {
+    constexpr T mix(F factor, T a, T b) {
         return conv<F, T>(mix(factor, conv<T, F>(a), conv<T, F>(b)));
     }
 
     template <class T, std::floating_point F = F32>
-    ColorRGB<T> mix(F factor, ColorRGB<T> a, ColorRGB<T> b) {
+    constexpr ColorRGB<T> mix(F factor, ColorRGB<T> a, ColorRGB<T> b) {
         return ColorRGB<T> {
             mix<T, F>(factor, a.r, b.r),
             mix<T, F>(factor, a.g, b.g),
