dc88035bca11d621230fbb8e66c7ae78f14b1eaa
Tom Savage <tcsavage@gmail.com>
2021-01-05 22:31:41 +0000

    Generalise generateHald to support float images

--- libs/image/libimage/include/image/luts/Hald.hpp
+++ libs/image/libimage/include/image/luts/Hald.hpp
@@ -9,24 +9,24 @@
 
 namespace image {
 
-    template <std::unsigned_integral T>
-    ImageBuf<T> generateHald(std::size_t sizeClass) {
+    template <class T1, std::unsigned_integral T2 = T1>
+    ImageBuf<T1> generateHald(std::size_t sizeClass) {
         std::size_t lutSize = sizeClass * sizeClass;
         std::size_t haldSize = lutSize * sizeClass;
-        std::size_t sizeBits = sizeof(T) * 8;
+        std::size_t sizeBits = sizeof(T2) * 8;
         std::size_t size = 1 << sizeBits;
         std::size_t step = size / lutSize;
 
-        NDArray<ColorRGB<T>> lut { Shape { lutSize, lutSize, lutSize }};
+        NDArray<ColorRGB<T1>> lut { Shape { lutSize, lutSize, lutSize }};
         for (std::size_t b = 0; b < lutSize; ++b) {
             for (std::size_t g = 0; g < lutSize; ++g) {
                 for (std::size_t r = 0; r < lutSize; ++r) {
-                    lut.at(r, g, b) = ColorRGB<T>(r * step, g * step, b * step);
+                    lut.at(r, g, b) = conv<T1, T2>(ColorRGB<T2>(r * step, g * step, b * step));
                 }
             }
         }
         auto haldShape = Shape { haldSize, haldSize };
-        return ImageBuf<T>{ lut.reshape(haldShape) };
+        return ImageBuf<T1>{ lut.reshape(haldShape) };
     }
 
 }
