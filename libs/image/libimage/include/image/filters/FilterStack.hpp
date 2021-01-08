8e88031a9fd414bf35e42311208b92e98fb33085
Tom Savage <tcsavage@gmail.com>
2021-01-08 19:57:47 +0000

    Make FilterStack pass-through precalculate flag

--- libs/image/libimage/include/image/filters/FilterStack.hpp
+++ libs/image/libimage/include/image/filters/FilterStack.hpp
@@ -9,24 +9,24 @@
 
 namespace image::filters {
 
-    template <class T, FilterImpl... Impls>
+    template <class T, bool Precalculate, FilterImpl... Impls>
     struct FilterStack {};
 
-    template <class T, FilterImpl Impl0, FilterImpl... RestImpls>
-    struct FilterStack<T, Impl0, RestImpls...> {
-        Filter<Impl0, T> item;
-        FilterStack<T, RestImpls...> next;
+    template <class T, bool Precalculate, FilterImpl Impl0, FilterImpl... RestImpls>
+    struct FilterStack<T, Precalculate, Impl0, RestImpls...> {
+        Filter<Impl0, T, Precalculate> item;
+        FilterStack<T, Precalculate, RestImpls...> next;
     };
 
-    template <FilterImpl Target, class T, FilterImpl Impl0, FilterImpl... RestImpls>
+    template <FilterImpl Target, class T, bool Precalculate, FilterImpl Impl0, FilterImpl... RestImpls>
     requires std::same_as<Target, Impl0>
-    constexpr Filter<Target, T> &get(FilterStack<T, Impl0, RestImpls...> &fs) noexcept {
+    constexpr Filter<Target, T, Precalculate> &get(FilterStack<T, Precalculate, Impl0, RestImpls...> &fs) noexcept {
         return fs.item;
     }
 
-    template <FilterImpl Target, class T, FilterImpl Impl0, FilterImpl... RestImpls>
+    template <FilterImpl Target, class T, bool Precalculate, FilterImpl Impl0, FilterImpl... RestImpls>
     requires (!std::same_as<Target, Impl0>)
-    constexpr Filter<Target, T> &get(FilterStack<T, Impl0, RestImpls...> &fs) noexcept {
+    constexpr Filter<Target, T, Precalculate> &get(FilterStack<T, Precalculate, Impl0, RestImpls...> &fs) noexcept {
         static_assert(sizeof...(RestImpls) > 0, "Filter implementation is not in this stack");
         return get<Target>(fs.next);
     }
