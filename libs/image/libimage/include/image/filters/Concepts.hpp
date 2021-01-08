5aa91bcd678cb6e91a22824e4f43b6129b4169be
Tom Savage <tcsavage@gmail.com>
2021-01-08 19:55:42 +0000

    Simplify requirements for FilterImpl
    Will want to revist this

--- libs/image/libimage/include/image/filters/Concepts.hpp
+++ libs/image/libimage/include/image/filters/Concepts.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
+#include <concepts>
+
 #include <image/Color.hpp>
 #include <image/CoreTypes.hpp>
 
 namespace image::filters {
 
     template <class F>
-    concept FilterImpl = requires(F filter, const ColorRGB<F32> &color) {
-        { filter.applyToColor(color) } noexcept -> std::same_as<ColorRGB<F32>>;
-    };
+    concept FilterImpl = std::default_initializable<F>;
 
 }
